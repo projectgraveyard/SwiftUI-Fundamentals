a71e9cbde00a5d69a67db0a4f67b417061118a43
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-04 20:26:43 +0100

    Create initial MySwiftUI app files

---
+++ MySwiftUI/MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by Dominic Swaine on 04/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

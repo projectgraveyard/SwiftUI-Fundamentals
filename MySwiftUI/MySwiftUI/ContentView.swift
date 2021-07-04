eb91507d88ef6a6bfd6e025bf0969050610bb97b
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-04 20:35:58 +0100

    Add modifiers to style the text, image views

--- MySwiftUI/MySwiftUI/ContentView.swift
+++ MySwiftUI/MySwiftUI/ContentView.swift
@@ -11,7 +11,14 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Image(systemName: "swift")
+                .resizable()
+                .frame(width: 100.0, height: 100.0)
+                .padding()
+                .background(Color.orange)
+                .padding([.leading, .bottom, .trailing])
             Text("Howdy, world!")
+                .fontWeight(.bold)
+                .kerning(5.0)
                 .padding()
         }
     }

a4b8bf94888541cda8da3889c20c103c728ac655
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-05 18:12:09 +0100

    Complete lesson 9 (Challenge: Stacks, ForEach)

--- Lesson-9/Weather Widget Challenge/ContentView.swift
+++ Lesson-9/Weather Widget Challenge/ContentView.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 
 struct ContentView: View {
@@ -37,26 +5,15 @@ struct ContentView: View {
   
   var body: some View {
     VStack(alignment: .leading, spacing: 20.0) {
-      Text("Cupertino")
-        .font(.title3)
-      Text("63°")
-        .font(.largeTitle)
-      
-      Image(systemName: "sun.max.fill")
-        .renderingMode(.original)
-      
-      Text("Sunny")
-      Text("H:68° L:42°")
-      
+      CurrentConditions()
       Divider()
-      
-      Text(weather.hourlyForecast[0].hour)
-      
-      weather.hourlyForecast[0].conditions
-        .renderingMode(.original)
-        .frame(height: 50)
-      
-      Text(weather.hourlyForecast[0].temperature)
+      HStack {
+        ForEach(weather.hourlyForecast) { hourlyForecast in
+          Spacer()
+          HourlyForecast(forecast: hourlyForecast)
+          Spacer()
+        }
+      }
     }
     .padding()
     .foregroundColor(.white)
@@ -67,7 +24,8 @@ struct ContentView: View {
 }
 
 struct Weather {
-  struct Forecast {
+  struct Forecast: Identifiable {
+    let id = UUID()
     let hour: String
     let conditions: Image
     let temperature: String
@@ -91,5 +49,38 @@ struct ContentView_Previews: PreviewProvider {
     
     ContentView()
       .previewLayout(.sizeThatFits)
+  }
+}
+
+struct CurrentConditions: View {
+  var body: some View {
+    HStack {
+      VStack(alignment: .leading) {
+        Text("Cupertino")
+          .font(.title3)
+        Text("63°")
+          .font(.largeTitle)
+      }
+      Spacer()
+      VStack(alignment: .trailing) {
+        Image(systemName: "sun.max.fill")
+          .renderingMode(.original)
+        Text("Sunny")
+        Text("H:68° L:42°")
+      }
+    }
+  }
+}
+
+struct HourlyForecast: View {
+  let forecast: Weather.Forecast
+  var body: some View {
+    VStack(spacing: 0) {
+      Text(forecast.hour)
+      forecast.conditions
+        .renderingMode(.original)
+        .frame(height: 50)
+      Text(forecast.temperature)
+    }
   }
 }
